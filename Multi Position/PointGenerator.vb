a1549a1bd2df6f7f7384baf3c570581f11249aa9
Gilles LE MARCHAND <gilles.le-marchand@univ-rennes1.fr>
2017-10-12 10:46:56 +0200

    Oublie d'ajout des fichiers

---
+++ Multi Position/PointGenerator.vb
@@ -0,0 +1,26 @@
+﻿Public Class PointGenerator
+    Private OnePoint As Point()
+    Private RArrey As ArrayList()
+
+    'Accès au prio
+    Public Property Point As Point()
+        Get
+            Return OnePoint
+        End Get
+        Set(value As Point())
+            OnePoint = value
+        End Set
+    End Property
+
+    Public Property ResultArray1 As ArrayList()
+        Get
+            Return RArrey
+        End Get
+        Set(value As ArrayList())
+            RArrey = value
+        End Set
+    End Property
+
+
+
+End Class
